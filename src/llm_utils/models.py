f23c26a53cad4df37c9b9baf0913116915dbbdfb
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-02-16 13:35:52 -0800

    add request timeout as arg instead of fixed to model choice

--- src/llm_utils/models.py
+++ src/llm_utils/models.py
@@ -6,35 +6,35 @@ registry = {
         "api_base": "https://api.openai.com/v1",
         "api_key_env_var": "OPENAI_API_KEY",
         "supports_json": True,
-        "request_timeout": 20,
+        # "request_timeout": 20,
     },
     "gpt-4-turbo": {
         "name": "gpt-4-0125-preview",
         "api_base": "https://api.openai.com/v1",
         "api_key_env_var": "OPENAI_API_KEY",
         "supports_json": True,
-        "request_timeout": 45,
+        # "request_timeout": 45,
     },
     "gpt-4": {
         "name": "gpt-4-0613",
         "api_base": "https://api.openai.com/v1",
         "api_key_env_var": "OPENAI_API_KEY",
         "supports_json": False,
-        "request_timeout": 45,
+        # "request_timeout": 45,
     },
     "mistral": {
         "name": "mistralai/Mistral-7B-Instruct-v0.1",
         "api_base": "https://api.endpoints.anyscale.com/v1",
         "api_key_env_var": "ANYSCALE_API_KEY",
         "supports_json": False,
-        "request_timeout": 45,
+        # "request_timeout": 45,
     },
     "mixtral": {
         "name": "mistralai/Mixtral-8x7B-Instruct-v0.1",
         "api_base": "https://api.endpoints.anyscale.com/v1",
         "api_key_env_var": "ANYSCALE_API_KEY",
         "supports_json": False,
-        "request_timeout": 60,
+        # "request_timeout": 60,
     },
 }
 
@@ -44,7 +44,7 @@ class APIModel:
     api_base: str
     api_key_env_var: str
     supports_json: bool
-    request_timeout: int
+    # request_timeout: int
 
     @classmethod
     def from_registry(cls, name: str):
