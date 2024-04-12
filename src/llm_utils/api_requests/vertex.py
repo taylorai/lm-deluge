3aa4ad4cf72cddd170bcf34477bb75a8c5081181
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-04-12 16:50:53 -0700

    vertex ai claude

--- src/llm_utils/api_requests/vertex.py
+++ src/llm_utils/api_requests/vertex.py
@@ -1,3 +1,4 @@
+# TODO: automatically distribute across regions for >> throughput :D
 import asyncio
 from aiohttp import ClientResponse
 import json
@@ -25,6 +26,9 @@ def get_access_token(service_account_file: str):
     return credentials.token
 
 class VertexAnthropicAPIRequest(APIRequestBase):
+    """
+    For Claude on Vertex, you'll also have to set the PROJECT_ID environment variable.
+    """
     def __init__(
         self,
         task_id: int,
@@ -55,28 +59,78 @@ class VertexAnthropicAPIRequest(APIRequestBase):
             result=result
         )
         self.model = APIModel.from_registry(model_name)
-        self.url = f"{self.model.api_base}/messages"
-
-        system_message = None
-        if len(self.messages) > 0 and self.messages[0]["role"] == "system":
-            system_message = self.messages.pop(0)
+        project_id = os.getenv("PROJECT_ID")
+        region = self.model.region
+        endpoint = f"https://{region}-aiplatform.googleapis.com"
+        self.url = f"{endpoint}/v1/projects/{project_id}/locations/{region}/publishers/anthropic/models/{model_name}:rawPredict"
         self.request_header = {
-            "x-api-key": os.getenv(self.model.api_key_env_var),
-            "anthropic-version": "2023-06-01",
-            "content-type": "application/json",
+            "Authorization": f"Bearer {get_access_token(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))}",
+            "Content-Type": "application/json"
         }
+        self.system_message = None
+        if len(self.messages) > 0 and self.messages[0]["role"] == "system":
+            self.system_message = self.messages.pop(0)
+
         self.request_json = {
-            "model": self.model.name,
+            "anthropic_version": "vertex-2023-10-16",
             "messages": self.messages,
             "temperature": self.sampling_params.temperature,
             "top_p": self.sampling_params.top_p,
             "max_tokens": self.sampling_params.max_new_tokens
         }
-        self.request_header = {
-            "Authorization": f"Bearer {os.getenv(self.model.api_key_env_var)}"
-        }
-        if system_message is not None:
-            self.request_json["system"] = system_message
+        if self.system_message is not None:
+            self.request_json["system"] = self.system_message
+
+    async def handle_response(self, response: ClientResponse) -> APIResponse:
+        is_error = False
+        error_message = None
+        completion = None
+        input_tokens = None
+        output_tokens = None
+        status_code = response.status
+        mimetype = response.headers.get("Content-Type", None)
+        if status_code >= 200 and status_code < 300:
+            try:
+                data = await response.json()
+                completion = data["content"][0]["text"]
+                input_tokens = data["usage"]["input_tokens"]
+                output_tokens = data["usage"]["output_tokens"]
+            except Exception as e:
+                is_error = True
+                error_message = f"Error calling .json() on response w/ status {status_code}"
+        elif "json" in mimetype.lower():
+            is_error = True # expected status is 200, otherwise it's an error
+            data = await response.json()
+            error_message = json.dumps(data)
+
+        else:
+            is_error = True
+            text = await response.text()
+            error_message = text
+
+
+        # handle special kinds of errors. TODO: make sure these are correct for anthropic
+        if is_error and error_message is not None:
+            if "rate limit" in error_message.lower() or "overloaded" in error_message.lower():
+                error_message += f" (Rate limit error, triggering cooldown.)"
+                self.status_tracker.time_of_last_rate_limit_error = time.time()
+                self.status_tracker.num_rate_limit_errors += 1
+            if "context length" in error_message:
+                error_message += f" (Context length exceeded, set retries to 0.)"
+                self.attempts_left = 0
+
+        return APIResponse(
+            status_code=status_code,
+            is_error=is_error,
+            error_message=error_message,
+            system_prompt=self.system_message,
+            messages=self.messages,
+            completion=completion,
+            model=self.model.name,
+            sampling_params=self.sampling_params,
+            input_tokens=input_tokens,
+            output_tokens=output_tokens,
+        )
 
 def convert_messages_to_contents(messages: list[dict]):
     contents = []
@@ -87,28 +141,6 @@ def convert_messages_to_contents(messages: list[dict]):
 class GeminiAPIRequest(APIRequestBase):
     """
     For Gemini, you'll also have to set the PROJECT_ID environment variable.
-    curl \
--X POST \
--H "Authorization: Bearer ***REMOVED***" \
--H "Content-Type: application/json" \
-https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:streamGenerateContent -d \
-$'{
-  "contents": {
-    "role": "user",
-    "parts": [
-      {
-      "fileData": {
-        "mimeType": "image/jpeg",
-        "fileUri": "gs://generativeai-downloads/images/scones.jpg"
-        }
-      },
-      {
-        "text": "Describe this picture."
-      }
-    ]
-  }
-}'
-
     """
     def __init__(
         self,
@@ -142,7 +174,8 @@ $'{
         credentials_file = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
         token = get_access_token(credentials_file)
         project_id = os.getenv("PROJECT_ID")
-        self.url = f"https://us-central1-aiplatform.googleapis.com/v1/projects/{project_id}/locations/us-central1/publishers/google/models/{model_name}:generateContent"
+        region = os.getenv("REGION", "us-central1")
+        self.url = f"https://{region}-aiplatform.googleapis.com/v1/projects/{project_id}/locations/{region}/publishers/google/models/{model_name}:generateContent"
 
         self.request_header = {
             "Authorization": f"Bearer {token}",
@@ -160,3 +193,26 @@ $'{
             },
             "safety_settings": {} # TODO: turn this off later lol
         }
+
+
+# curl \
+# -X POST \
+# -H "Authorization: Bearer ***REMOVED***" \
+# -H "Content-Type: application/json" \
+# https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/${MODEL_ID}:streamGenerateContent -d \
+# $'{
+#   "contents": {
+#     "role": "user",
+#     "parts": [
+#       {
+#       "fileData": {
+#         "mimeType": "image/jpeg",
+#         "fileUri": "gs://generativeai-downloads/images/scones.jpg"
+#         }
+#       },
+#       {
+#         "text": "Describe this picture."
+#       }
+#     ]
+#   }
+# }'
