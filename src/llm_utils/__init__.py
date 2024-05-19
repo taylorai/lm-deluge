ae43704e3ba0481e405624ca7610c65a3c2fa873
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-05-18 20:13:30 -0700

    dry run

--- src/llm_utils/__init__.py
+++ src/llm_utils/__init__.py
@@ -11,10 +11,11 @@ from .tracker import StatusTracker
 from .sampling_params import SamplingParams
 from .models import registry
 from .api_requests.base import APIResponse
-from .utils import instructions_to_message_lists
+from .utils import instructions_to_message_lists, dry_run
 from .api_requests import create_api_request
 ModalLLMClient = modal.Cls.lookup("llm-utils", "ModalLLMClient")
 
+# TODO: dry run to estimate costs
 
 @dataclass
 class ClientConfig:
@@ -145,12 +146,13 @@ class LLMClient:
             request_timeout=self.request_timeout,
             sampling_params=self.sampling_params
         )
-
+    
     async def process_prompts_async(
         self,
         prompts: Union[list[str], list[list[dict]]],
         return_completions_only: bool = False,
-        show_progress=True
+        show_progress: bool = True,
+        dry_run: bool = False
     ):
         # if prompts are strings, convert them to message lists
         if isinstance(prompts[0], str):
@@ -169,7 +171,8 @@ class LLMClient:
         api_ids = [i for i in ids if i not in modal_ids]
         print(f"Processing {len(modal_ids)} modal prompts and {len(api_ids)} api prompts.")
 
-        # create async tasks for each
+
+        # decide which prompts go to which models
         modal_prompts = [prompts[i] for i in modal_ids]
         api_prompts = [prompts[i] for i in api_ids]
         modal_models = [model for model in self.models if registry[model]["api_spec"] == "modal"]
@@ -178,9 +181,24 @@ class LLMClient:
         api_models = [model for model in self.models if registry[model]["api_spec"] != "modal"]
         api_weights = [self.model_weights[i] for i, model in enumerate(self.models) if registry[model]["api_spec"] != "modal"]
         api_sampling_params = [self.sampling_params[i] for i, model in enumerate(self.models) if registry[model]["api_spec"] != "modal"]
-        
+
         modal_task = None
         api_task = None
+        if dry_run:
+            results = api_prompts_dry_run(
+                ids, api_prompts, api_models, api_weights, api_sampling_params,
+                max_attempts=self.max_attempts,
+                max_tokens_per_minute=self.max_tokens_per_minute,
+                max_requests_per_minute=self.max_requests_per_minute,
+                request_timeout=self.request_timeout,
+                progress_bar=pbar,
+                use_qps=self.use_qps,
+                debug=self.debug
+            )
+            print("Dry run results for API models (does not include Modal):")
+            print(results)
+            return results
+        
         if len(modal_prompts) > 0:
             modal_task = asyncio.create_task(
                 process_modal_prompts_async(
@@ -242,7 +260,7 @@ async def process_modal_prompts_async(
     model_weights: list[float],
     sampling_params: list[SamplingParams],
     batch_size: int = 1_000,
-    progress_bar: Optional[tqdm] = None,
+    progress_bar: Optional[tqdm] = None
 ):
     # change ids to integer list
     if isinstance(ids, np.ndarray):
@@ -255,6 +273,7 @@ async def process_modal_prompts_async(
     if len(ids) != len(prompts):
         raise ValueError("ids and prompts must be the same length.")
     
+    # if dry run, just directly create list of APIResponse objects with no completion and return them
     # look up the models
     completion_fns = [
         f'{registry[model]["name"]}-completions-{registry[model]["gpus"][0]}' for model in models
@@ -286,6 +305,64 @@ async def process_modal_prompts_async(
         APIResponse(**r) for r in results
     ]
 
+def api_prompts_dry_run(
+    ids: Union[np.ndarray, list[int]],
+    prompts: list[list[dict]],  # each prompt is just a list of messages
+    models: Union[str, list[str]],
+    model_weights: list[float],
+    sampling_params: list[SamplingParams],
+    max_attempts: int = 5,
+    max_tokens_per_minute: int = 500_000,
+    max_requests_per_minute: int = 1_000,
+    request_timeout: int = 30,
+    progress_bar: Optional[tqdm] = None,
+    use_qps: bool = False,
+    debug: bool = False
+):
+    """
+    Count tokens and estimate costs for a batch of prompts.
+    """
+    results = []
+    for i, messages in zip(ids, prompts):
+        # choose a model
+        model_idx = np.random.choice(range(len(models)), p=model_weights)
+        model = models[model_idx]
+
+        # dry run
+        input_tokens, output_tokens, min_cost, max_cost = dry_run(
+            model, messages, sampling_params[model_idx].max_new_tokens
+        )
+        results.append({
+            "id": i,
+            "input_tokens": input_tokens,
+            "output_tokens": output_tokens,
+            "min_cost": min_cost,
+            "max_cost": max_cost
+        })
+
+    results = {
+        "total_input_tokens": sum([r["input_tokens"] for r in results]),
+        "total_output_tokens": sum([r["output_tokens"] for r in results]),
+        "total_min_cost": sum([r["min_cost"] for r in results]),
+        "total_max_cost": sum([r["max_cost"] for r in results]),
+    }
+    minimum_time_tpm = results["total_input_tokens"] / max_tokens_per_minute
+    maximum_time_tpm = (results["total_input_tokens"] + results["total_output_tokens"]) / max_tokens_per_minute
+    minimum_time_rpm = len(prompts) / max_requests_per_minute
+
+    results["minimum_time"] = max(minimum_time_tpm, minimum_time_rpm)
+    results["maximum_time"] = max(maximum_time_tpm, minimum_time_rpm)
+    limiting_factor = None
+    if minimum_time_rpm > maximum_time_tpm:
+        limiting_factor = "requests"
+    elif minimum_time_rpm < minimum_time_tpm:
+        limiting_factor = "tokens"
+    else:
+        limiting_factor = "depends"
+    results["limiting_factor"] = limiting_factor
+    
+    return results
+
 async def process_api_prompts_async(
     ids: Union[np.ndarray, list[int]],
     prompts: list[list[dict]],  # each prompt is just a list of messages
@@ -298,7 +375,7 @@ async def process_api_prompts_async(
     request_timeout: int = 30,
     progress_bar: Optional[tqdm] = None,
     use_qps: bool = False,
-    debug: bool = False,
+    debug: bool = False
 ):
     """Processes API requests in parallel, throttling to stay under rate limits."""
     # change ids to integer list
@@ -540,3 +617,139 @@ class RemoteLLMClient:
             max_attempts=config.max_attempts,
             request_timeout=config.request_timeout
         )
+    
+class BatchLLMClient:
+    def __init__(
+        self, 
+        model_names: list[str],
+        sampling_params: Union[SamplingParams, list[SamplingParams]] = SamplingParams()
+    ):
+        if len(model_names) > 1:
+            raise ValueError("BatchLLMClient only supports a single model.")
+        model = model_names[0]
+        if registry.get(model, {}).get("api_spec", None) != "openai":
+            raise ValueError("BatchLLMClient only supports OpenAI models.")
+        
+        self.openai_model = registry[model]["name"]
+        self.sampling_params = sampling_params
+
+    def process_prompts_sync(
+        self,
+        prompts: Union[list[str], list[list[dict]]],
+        return_completions_only: bool = False,
+        show_progress=True
+    ):
+        import asyncio
+        return asyncio.run(
+            self.process_prompts_async(
+                prompts=prompts,
+                return_completions_only=return_completions_only,
+                show_progress=show_progress
+            )
+        )
+        
+    async def process_prompts_async(
+        self,
+        prompts: Union[list[str], list[list[dict]]],
+        return_completions_only: bool = False,
+        show_progress=True
+    ):
+        import os
+        from .api_requests.base import APIResponse
+        import pandas as pd
+        import requests
+
+        # if prompts are strings, convert them to message lists
+        if isinstance(prompts[0], str):
+            prompts = instructions_to_message_lists(prompts)
+        ids = np.arange(len(prompts))
+
+        # create file with requests to send to batch api
+        batch_requests = []
+        for id, prompt in zip(ids, prompts):
+            batch_requests.append({
+                "custom_id": str(id),
+                "method": "POST",
+                "url": "/v1/chat/completions",
+                "body": {
+                    "model": self.openai_model,
+                    "messages": prompt,
+                    "max_tokens": self.sampling_params.max_new_tokens,
+                    "temperature": self.sampling_params.temperature,
+                    "top_p": self.sampling_params.top_p,
+                }
+            })
+
+        # save the file
+        pd.DataFrame(batch_requests).to_json(
+            "openai_requests_temp.jsonl", orient="records", lines=True
+        )
+
+        # upload the file
+        api_key = os.environ.get("OPENAI_API_KEY", None)
+        if api_key is None:
+            raise ValueError("OPENAI_API_KEY environment variable must be set.")
+        url = 'https://api.openai.com/v1/files'
+        files = {
+            'file': ("openai_requests_temp.jsonl", open("openai_requests_temp.jsonl", 'rb')),
+        }
+        data = {
+            'purpose': 'batch',
+        }
+        headers = {
+            'Authorization': f'Bearer {api_key}',
+        }
+        response = requests.post(url, files=files, data=data, headers=headers)
+
+        file_id = None
+        if response.status_code == 200:
+            print('File uploaded successfully')
+            data = response.json()
+            file_id = data['id']
+
+        else:
+            print('File upload failed')
+            raise ValueError(f"Error uploading file: {response.text}")
+        
+        # start batch completions job
+        # curl https://api.openai.com/v1/batches \
+        #   -H "Authorization: Bearer $OPENAI_API_KEY" \
+        #   -H "Content-Type: application/json" \
+        #   -d '{
+        #     "input_file_id": "file-abc123",
+        #     "endpoint": "/v1/chat/completions",
+        #     "completion_window": "24h"
+        #   }'
+        url = 'https://api.openai.com/v1/batches'
+        data = {
+            'input_file_id': file_id,
+            'endpoint': '/v1/chat/completions',
+            'completion_window': '24h'
+        }
+        response = requests.post(url, json=data, headers=headers)
+
+        batch_id = None
+        if response.status_code == 200:
+            print('Batch job started successfully')
+            data = response.json()
+            batch_id = data['id']
+
+        else:
+            print('Batch job failed to start')
+            raise ValueError(f"Error starting batch job: {response.text}")
+        
+        # every 30s check job status. give up after 24h
+        for t in range(24 * 60 * 2):
+            print(f"Checking job status, attempt {t}")
+            response = requests.get(
+                f"https://api.openai.com/v1/batches/{batch_id}", headers=headers)
+            if response.status_code == 200:
+                data = response.json()
+                print(data)
+                if data['status'] == "completed":
+                    print('Job completed successfully')
+                    break
+            else:
+                print('Error checking job status')
+                raise ValueError(f"Error checking job status: {response.text}")
+            await asyncio.sleep(30)
