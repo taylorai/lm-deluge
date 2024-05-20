75d0fb0ffd81a903f2b3a38d4ae3d6a006c21514
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-05-19 21:02:17 -0700

    try to improve remote client

--- modal_deployments/remote_client.py
+++ modal_deployments/remote_client.py
@@ -26,8 +26,9 @@ app = App("llm-utils")
         Secret.from_name("OPENAI_API_KEY"),
         Secret.from_name("ANTHROPIC_API_KEY"),
         Secret.from_name("COHERE_API_KEY"),
+         Secret.from_name("my-googlecloud-secret")
     ],
-    concurrency_limit=10,
+    concurrency_limit=1,
     timeout=60 * 60 * 24
 )
 class ModalLLMClient:
@@ -39,16 +40,20 @@ class ModalLLMClient:
     def process_prompts(self, prompts: Union[list[str], list[list[dict]]]):
         import json
         from llm_utils import APIResponse
+        if os.getenv("SERVICE_ACCOUNT_JSON", None) is not None:
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/service_account.json"
+            json.dump(json.loads(os.getenv("SERVICE_ACCOUNT_JSON")), open("/service_account.json", "w"))
+    
         print("Processing prompts...")
         result: list[APIResponse] = self.client.process_prompts_sync(prompts)
-        result_json = [r.to_dict() for r in result]
-        # log result to volume in case it's lost.
-        # file should start with HH-mm-ss-MM-DD-YYYY
-        print("Writing results to file...")
         now = datetime.datetime.now().strftime('%H-%M-%S-%m-%d-%Y')
-        filename = f"/outputs/results-{now}.json"
+        filename = f"/outputs/results-{now}.jsonl"
         with open(filename, "w") as f:
-            f.write(json.dumps(result_json))
+            for i, r in enumerate(result):
+                result_json = r.to_dict()
+                f.write(json.dumps(result_json) + "\n")
+                if i % 10_000 == 0:
+                    vol.commit()
+                yield result_json    
         vol.commit()
         print("Done!")
-        return result_json
